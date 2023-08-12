0daf00557f8f129278c5af23c06d22a9fce41818
TERAsu03 <115889714+TERAsu03@users.noreply.github.com>
2023-08-12 22:07:20 +0200

    Update Serveur_non_sure.c
    Mise en place des arguments

--- Serveur_non_sure.c
+++ Serveur_non_sure.c
@@ -7,65 +7,84 @@
 
 #define MAX_COMMAND_LENGTH 4096
 
-int main() {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <serveur_ip> <serveur_port>\n", argv[0]);
-        exit(1);
-    }
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddress, clientAddress;
+int main(int arc, char *argv[]) {
+   if (arc != 3) {
+      printf("Usage: %s <server_ip><serveur_port\n", argv[0]);
+      return(1);
+   }
+   int serverSocket, clientSocket;
+   struct sockaddr_in serverAddress, clientAddress;
+   socklen_t clientLength;
 
-    //Création du socket client
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        perror("Erreur lors de la création du socket");
-        exit(1);
-    }
+   //Création du socket
+   serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+   if (serverSocket == -1) {
+      perror("Erreur lors de la création du socket");
+      exit(1);
+   }
+   
+   // Configuration de l'adresse du serveur
+   serverAddress.sin_family = AF_INET;
+    int port = atoi(argv[2]);
+   serverAddress.sin_port = htons(port); //Numéro de port
+   serverAddress.sin_addr.s_addr = inet_addr(argv[1]); // Adresse IP spécifique
+   
+   // Effectuer la liaison du socket du serveur avec une adresse IP et un port spécifiques
+   if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+      perror("Erreur lors de la liaison du socket");
+      exit(1);
+   }
 
-    // Configuration de l'adresse du serveur
-// Configuration de l'adresse du serveur
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(1234); // Numéro de port
-    serverAddress.sin_addr.s_addr = inet_addr("192.168.1.40"); // Utilisation de la variable server_ip
+   
+   //Mettre le socket en écoute
+   if (listen(serverSocket, 3) < 0) {
+      perror("Erreur lors de l'écoute");
+      exit(1);
+   }
+   
+   //Acceptation de la connexion
+   if ((clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, (socklen_t*)&clientLength))<0) {
+      perror("Erreur lors de l'acceptation de la connexion");
+      exit(1);
+   }
 
 
+   //Boucle de communication du serveur
+   while(1) {
+      char command[MAX_COMMAND_LENGTH];
+      memset(command, 0, sizeof(command));
 
-    
-    //Connexion au serveur
-    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
-        perror("Erreur lors de la connexion au serveur");
-        exit(1);
-    }
+      //Reception de la commande
+      if (recv(clientSocket, command, sizeof(command), 0) <= 0) {
+         perror("Erreur lors de la réception de la commande");
+         exit(1);   
+      }
 
-    //Boucle de communication du client
-    char command[MAX_COMMAND_LENGTH];
-    while (1) {
-        printf("Entrez une commande : ");
-        fgets(command, MAX_COMMAND_LENGTH, stdin);
-        //Suppression le saut a la ligne de la commande
-        command[strcspn(command, "\n")] = 0; 
-        //Envoi de la commande au serveur
-        if (send(clientSocket, command, strlen(command), 0) < 0) {
-            perror("Erreur lors de l'envoi de la commande");
+      //Exécution de la commande système
+      FILE* fp = popen(command, "r");
+      if (fp == NULL) {
+         perror("Erreur lors de l'exécution de la commande");
+         break;
+      }
+
+      //Lecture de la sortie de la commande
+      char output[4096]; 
+      memset(output, 0, sizeof(output));
+      while (fgets(output, sizeof(output), fp) != NULL) {
+         //Envoi de la sortie de la commande
+         if (send(clientSocket, output, strlen(output), 0) == -1) {
+            perror("Erreur lors de l'envoi de la sortie de la commande");
             break;
-        }
-        
-        //Réception de la réponse du serveur
-        char output[MAX_COMMAND_LENGTH];
-        memset(output, 0, sizeof(output));
-        int bytesReceived = recv(clientSocket, output, sizeof(output) -1, 0);
-        if (bytesReceived == -1 ) {
-            perror("Erreur lors de la réception de la réponse");
-            break;
-        } else if (bytesReceived == 0) {
-            printf("Déconnexion du serveur\n");
-            break;
-        } else {
-            printf("Réponse du serveur : %s\n", output);
-        }   
-    }
+         }
+         memset(output, 0, sizeof(output));
+      }
+      //Envoi du resultat de la commande
+      if (send(clientSocket, "Fin de la commande", 19, 0) == -1) {
+         perror("Erreur lors de l'envoi du résultat de la commande");
+         break;
+      }
 
-    //Fermeture du socket
-    close(clientSocket);
-    return 0;
+      //Terminer la commande
+      pclose(fp);
+   }
 }
