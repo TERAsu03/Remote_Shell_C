894f51d65b281a75eb87ba00029080a28a01e0aa
TERAsu03 <115889714+TERAsu03@users.noreply.github.com>
2023-08-13 17:51:16 +0200

    Update Client.c
    Correction faute orthographe

--- Client.c
+++ Client.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <serveur_ip> <serveur_port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
         exit(1);
     }
     int serverSocket, clientSocket;
